a7ba8bc068339e1e5f2072af6ba3aeb91b450acc
Lymer07 <113987843+Lymer07@users.noreply.github.com>
2022-09-20 12:44:21 +0100

    Update aws_test.tf

--- terraformcode/aws_test.tf
+++ terraformcode/aws_test.tf
@@ -2,17 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-west-2"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
